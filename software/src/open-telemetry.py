2c02bf20796ee7fc88d2829bcd341a143d72b4c1
Larry W Jordan Jr <larouex@gmail.com>
2022-10-21 13:07:56 -0700

    Added v.0.9 Open Telemetry for Testing #6

--- software/src/open-telemetry.py
+++ software/src/open-telemetry.py
@@ -24,9 +24,7 @@ log_emitter_provider = LogEmitterProvider()
 set_log_emitter_provider(log_emitter_provider)
 
 exporter = AzureMonitorLogExporter(
-    connection_string=os.environ[
-        "InstrumentationKey=beec3397-2433-489c-833a-e4b9e4c4a1f6;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
-    ]
+    connection_string="InstrumentationKey=beec3397-2433-489c-833a-e4b9e4c4a1f6;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
 )
 
 log_emitter_provider.add_log_processor(BatchLogProcessor(exporter))
