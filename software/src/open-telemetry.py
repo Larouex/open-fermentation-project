68b23c7021c6bcfcf90e25a302a672fe933f3939
Larry W Jordan Jr <larouex@gmail.com>
2022-10-21 13:12:15 -0700

    Added v.0.9 Open Telemetry for Testing #7

--- software/src/open-telemetry.py
+++ software/src/open-telemetry.py
@@ -8,32 +8,67 @@
 #   (c) 2022 Larouex Gourmet Foods LLC
 #   This code is licensed under GNU license (see LICENSE.txt for details)
 # ==================================================================================
+"""
+An example to show an application using all instruments in the OpenTelemetry SDK. Metrics created
+and recorded using the sdk are tracked and telemetry is exported to application insights with the
+AzureMonitorMetricsExporter.
+"""
 import os
-import logging
+from typing import Iterable
 
-from opentelemetry.sdk._logs import (
-    LogEmitterProvider,
-    LoggingHandler,
-    set_log_emitter_provider,
-)
-from opentelemetry.sdk._logs.export import BatchLogProcessor
+from opentelemetry import metrics
+from opentelemetry.metrics import CallbackOptions, Observation
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
-from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter
+from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
 
-log_emitter_provider = LogEmitterProvider()
-set_log_emitter_provider(log_emitter_provider)
-
-exporter = AzureMonitorLogExporter(
+exporter = AzureMonitorMetricExporter(
     connection_string="InstrumentationKey=beec3397-2433-489c-833a-e4b9e4c4a1f6;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
 )
+reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
+metrics.set_meter_provider(MeterProvider(metric_readers=[reader]))
 
-log_emitter_provider.add_log_processor(BatchLogProcessor(exporter))
-handler = LoggingHandler()
+# Create a namespaced meter
+meter = metrics.get_meter_provider().get_meter("sample")
 
-# Attach LoggingHandler to root logger
-logging.getLogger().addHandler(handler)
-logging.getLogger().setLevel(logging.NOTSET)
+# Callback functions for observable instruments
+def observable_counter_func(options: CallbackOptions) -> Iterable[Observation]:
+    yield Observation(1, {})
 
-logger = logging.getLogger(__name__)
 
-logger.warning("Hello World!")
+def observable_up_down_counter_func(
+    options: CallbackOptions,
+) -> Iterable[Observation]:
+    yield Observation(-10, {})
+
+
+def observable_gauge_func(options: CallbackOptions) -> Iterable[Observation]:
+    yield Observation(9, {})
+
+
+# Counter
+counter = meter.create_counter("counter")
+counter.add(1)
+
+# Async Counter
+observable_counter = meter.create_observable_counter(
+    "observable_counter", [observable_counter_func]
+)
+
+# UpDownCounter
+up_down_counter = meter.create_up_down_counter("up_down_counter")
+up_down_counter.add(1)
+up_down_counter.add(-5)
+
+# Async UpDownCounter
+observable_up_down_counter = meter.create_observable_up_down_counter(
+    "observable_up_down_counter", [observable_up_down_counter_func]
+)
+
+# Histogram
+histogram = meter.create_histogram("histogram")
+histogram.record(99.9)
+
+# Async Gauge
+gauge = meter.create_observable_gauge("gauge", [observable_gauge_func])
