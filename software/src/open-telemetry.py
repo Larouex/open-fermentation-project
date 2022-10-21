337eba5016f3f4802ca510e0d21fc775002d52eb
Larry W Jordan Jr <larouex@gmail.com>
2022-10-21 12:21:21 -0700

    Added v.0.9 Open Telemetry for Testing #3

--- software/src/open-telemetry.py
+++ software/src/open-telemetry.py
@@ -9,11 +9,17 @@
 #   This code is licensed under GNU license (see LICENSE.txt for details)
 # ==================================================================================
 import getopt, sys, time, string, threading, asyncio, os
-import logging as Log
+import logging
 
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import (
+    SERVICE_NAME,
+    SERVICE_NAMESPACE,
+    SERVICE_INSTANCE_ID,
+    Resource,
+)
 
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 
@@ -21,6 +27,32 @@ exporter = AzureMonitorTraceExporter.from_connection_string(
     "InstrumentationKey=beec3397-2433-489c-833a-e4b9e4c4a1f6;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
 )
 
+
+logging.basicConfig(format="%(asctime)s:%(levelname)s:%(message)s", level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+file = logging.FileHandler("example.log")
+stream = logging.StreamHandler()
+logger.addHandler(file)
+logger.addHandler(stream)
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create(
+            {
+                SERVICE_NAME: "saluminator-fermentation-01",
+                # ----------------------------------------
+                # Setting role name and role instance
+                # ----------------------------------------
+                SERVICE_NAMESPACE: "saluminatorFermentationSystem",
+                SERVICE_INSTANCE_ID: "1",
+                # ----------------------------------------------
+                # Done setting role name and role instance
+                # ----------------------------------------------
+            }
+        )
+    )
+)
 trace.set_tracer_provider(TracerProvider())
 tracer = trace.get_tracer(__name__)
 span_processor = BatchSpanProcessor(exporter)
