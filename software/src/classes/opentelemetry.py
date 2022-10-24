f1b1f12e0f4811e5025b523b0612cae7dae12cd0
Larry W Jordan Jr <larouex@gmail.com>
2022-10-24 12:45:40 -0700

    Added v.0.9 Open Telemetry for Testing #11

---
+++ software/src/classes/opentelemetry.py
@@ -0,0 +1,113 @@
+# ==================================================================================
+#   File:   opentelemetry.py
+#   Author: Larry W Jordan Jr (larouex@larouex.com)
+#   Use:    Creates the monitoring and instrumentation of the Saluminator and
+#           sends the data to Azure Application Insights
+#
+#   https://github.com/Larouex/open-fermentation-project
+#
+#   (c) 2022 Larouex Gourmet Foods LLC
+#   This code is licensed under GNU license (see LICENSE.txt for details)
+# ==================================================================================
+import json, sys, time, string, threading, asyncio, os, copy
+import logging
+
+# our classes
+from classes.config import Config
+from classes.secrets import Secrets
+
+from typing import Iterable
+from opentelemetry import metrics
+from opentelemetry.metrics import CallbackOptions, Observation
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk._logs.export import BatchLogProcessor
+
+# Experimental Logging Export
+from opentelemetry.sdk._logs import (
+    LogEmitterProvider,
+    LoggingHandler,
+    set_log_emitter_provider,
+)
+
+from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter
+from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+
+
+class OpenTelemetry:
+    def __init__(self):
+
+        # load up configuration and mapping files
+        self.config = []
+        self.nodes = []
+        self.load_config()
+
+        # Load the secrets file
+        self.secrets = []
+        self.load_secrets()
+
+        # Logging
+        log_emitter_provider = LogEmitterProvider()
+        set_log_emitter_provider(log_emitter_provider)
+
+        # Create the Various Monitor Types
+        log_exporter = AzureMonitorLogExporter(
+            connection_string=self.secrets["ApplicationInsightsConnectionString"]
+        )
+
+        metric_exporter = AzureMonitorMetricExporter(
+            connection_string=self.secrets["ApplicationInsightsConnectionString"]
+        )
+
+        trace_exporter = AzureMonitorTraceExporter(
+            connection_string=self.secrets["ApplicationInsightsConnectionString"]
+        )
+
+        # Attach LoggingHandler to root logger
+        log_emitter_provider.add_log_processor(BatchLogProcessor(log_exporter))
+        handler = LoggingHandler()
+        self.logger.getLogger().addHandler(handler)
+        self.logger.getLogger().setLevel(self.logger.NOTSET)
+
+    # -------------------------------------------------------------------------------
+    #   Function:   export_log
+    #   Usage:      Send a logging event to the Azure Monitor Exporter
+    # -------------------------------------------------------------------------------
+    async def export_log(self):
+
+        try:
+
+            logger = self.logger.getLogger(__name__)
+            logger.warning("Hello World!")
+
+        except Exception as ex:
+            self.logger.error("[ERROR] %s" % ex)
+            self.logger.error("[TERMINATING] We encountered an error in Gateway")
+            return
+
+        return logger
+
+    # -------------------------------------------------------------------------------
+    #   Function:   load_config
+    #   Usage:      Loads the configuration from file and setup iterators for
+    #               sending telemetry in sequence
+    # -------------------------------------------------------------------------------
+    def load_config(self):
+
+        # Load all the configuration
+        config = Config(self.logger)
+        self.config = config.data
+        self.nodes = self.config["Nodes"]
+        return
+
+    # -------------------------------------------------------------------------------
+    #   Function:   load_secrets
+    #   Usage:      Loads the secrets
+    # -------------------------------------------------------------------------------
+    def load_secrets(self):
+
+        # Load all the configuration
+        secrets = Secrets(self.logger)
+        self.secrets = secrets.data
+        return
--- software/src/open-telemetry.py
+++ software/src/open-telemetry.py
@@ -8,74 +8,9 @@
 #   (c) 2022 Larouex Gourmet Foods LLC
 #   This code is licensed under GNU license (see LICENSE.txt for details)
 # ==================================================================================
-"""
-An example to show an application using all instruments in the OpenTelemetry SDK. Metrics created
-and recorded using the sdk are tracked and telemetry is exported to application insights with the
-AzureMonitorMetricsExporter.
-"""
 import os, time
 from typing import Iterable
 
-from opentelemetry import metrics
-from opentelemetry.metrics import CallbackOptions, Observation
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from classes.opentelemetry import OpenTelemetry
 
-from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
-
-exporter = AzureMonitorMetricExporter(
-    connection_string="InstrumentationKey=beec3397-2433-489c-833a-e4b9e4c4a1f6;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
-)
-reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
-metrics.set_meter_provider(MeterProvider(metric_readers=[reader]))
-
-# Create a namespaced meter
-meter = metrics.get_meter_provider().get_meter("saluminator")
-
-# Callback functions for observable instruments
-def observable_counter_func(options: CallbackOptions) -> Iterable[Observation]:
-    yield Observation(1, {})
-
-
-def observable_up_down_counter_func(
-    options: CallbackOptions,
-) -> Iterable[Observation]:
-    yield Observation(-10, {})
-
-
-def observable_gauge_func(options: CallbackOptions) -> Iterable[Observation]:
-    yield Observation(9, {})
-
-
-# Counter
-counter = meter.create_counter("counter")
-
-
-# Async Counter
-observable_counter = meter.create_observable_counter(
-    "observable_counter", [observable_counter_func]
-)
-
-# Async UpDownCounter
-observable_up_down_counter = meter.create_observable_up_down_counter(
-    "observable_up_down_counter", [observable_up_down_counter_func]
-)
-
-histogram = meter.create_histogram("histogram")
-
-up_down_counter = meter.create_up_down_counter("up_down_counter")
-
-gauge = meter.create_observable_gauge("gauge", [observable_gauge_func])
-
-for x in range(0, 300):
-    counter.add(1)
-
-    # UpDownCounter
-    up_down_counter.add(1)
-    up_down_counter.add(-5)
-
-    # Histogram
-    histogram.record(99.9)
-
-    # Async Gauge
-    time.sleep(30)
+logger = OpenTelemetry.export_log()
